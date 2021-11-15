69e34d27c5a2eb1dc1f2967d5eb7b50f43042d03
Pedro Bassetto <pedrobassetto1997@gmail.com>
2021-11-15 11:52:22 -0300

    Finalizado versão beta sem estrutura de handler

---
+++ investment/src/main/java/br/com/lab/impacta/investment/service/InvestmentService.java
@@ -0,0 +1,9 @@
+package br.com.lab.impacta.investment.service;
+
+import br.com.lab.impacta.investment.model.Investment;
+
+public interface InvestmentService {
+
+    Investment invest(Long productId, Long accountId, Double valueInvestment);
+
+}
