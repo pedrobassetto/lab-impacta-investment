4442125d3bf0a51441f45607a15c8649b89d9d6d
Pedro Bassetto <pedrobassetto1997@gmail.com>
2021-11-20 10:25:45 -0300

    Refatorado para Handler Exception

---
+++ investment/src/main/java/br/com/lab/impacta/investment/handler/exception/InvestmentProductDontExistException.java
@@ -0,0 +1,19 @@
+package br.com.lab.impacta.investment.handler.exception;
+
+public class InvestmentProductDontExistException extends RuntimeException {
+
+    private String description;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public InvestmentProductDontExistException() {
+    }
+
+    public InvestmentProductDontExistException(String message, String description) {
+        super(message);
+        this.description = description;
+    }
+
+}
